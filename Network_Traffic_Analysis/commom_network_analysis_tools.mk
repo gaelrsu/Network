20e7d0d734ca5dbca2b3f1ec8601959b13daea1d
gaelrsu <58053315+gaelrsu@users.noreply.github.com>
2024-04-12 13:29:43 +0200

    Create commom_network_analysis_tools.mk

---
+++ Network_Traffic_Analysis/commom_network_analysis_tools.mk
@@ -0,0 +1,17 @@
+# title
+
+## commom network analysis tools 
+
+| Tool              | Description                                                                                                                                          |
+|-------------------|------------------------------------------------------------------------------------------------------------------------------------------------------|
+| tcpdump           | tcpdump is a command-line utility that, with the aid of LibPcap, captures and interprets network traffic from a network interface or capture file.  |
+| Tshark            | TShark is a network packet analyzer much like TCPDump. It will capture packets from a live network or read and decode from a file.                 |
+| Wireshark         | Wireshark is a graphical network traffic analyzer. It captures and decodes frames off the wire and allows for an in-depth look into the environment. |
+| NGrep             | NGrep is a pattern-matching tool built to serve a similar function as grep for Linux distributions. It works with network traffic packets.           |
+| tcpick            | tcpick is a command-line packet sniffer that specializes in tracking and reassembling TCP streams.                                                |
+| Network Taps      | Taps (Gigamon, Niagra-taps) are devices capable of taking copies of network traffic and sending them to another place for analysis.                    |
+| Networking Span Ports | Span Ports copy frames from layer two or three networking devices and send them to a collection point for analysis.                                |
+| Elastic Stack     | The Elastic Stack ingests data from many sources, visualizes it, and enables searching and analysis.                                                |
+| SIEMS             | SIEMS (such as Splunk) are central points where data is analyzed, visualized, and used for alerting and forensic analysis.                            |
+
+
